0beb11834cba96f6730e9873d10a8c68ce0e1d00
mehmetmalgir <mmalgr@gmail.com>
2023-02-07 23:49:07 +0300

    ProductForCreation Dto Create

--- MlgStore.Model/Dtos/ProductForCreation.cs
+++ MlgStore.Model/Dtos/ProductForCreation.cs
@@ -6,18 +6,19 @@ namespace MlgStore.Model.Dtos
 {
     public class ProductForCreation
     {
-        
+
         public string ProductName { get; set; }
         public string ProductDescription { get; set; }
+        public int? CategoryID { get; set; }
         public int? QuantityPerUnit { get; set; }
-        public int? UnitPrice { get; set; }        
-        public string CategoryName { get; set; }
-        public string Description { get; set; }
-        public string Size { get; set; }
-        public string Color { get; set; }
-        public string GenderType { get; set; }
+        public int? UnitPrice { get; set; }
+        public int? SizeID { get; set; }
+        public int? ColorID { get; set; }
         public int? Discount { get; set; }
-        public string Picture { get; set; }
+        public bool DiscountAvailable { get; set; }
+        public bool CurrentOrder { get; set; }
+        public int GenderID { get; set; }
+        public string ProductPhoto { get; set; }
 
 
 
